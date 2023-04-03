ab3629e88f81360e02b7182d5f2ad2549718af4d
Michaelszeng <michaelszeng@gmail.com>
2023-04-02 18:31:38 -0700

    map generation working

--- RRT.py
+++ RRT.py
@@ -4,27 +4,51 @@ Helper functions and classes for RRT* algorithm
 import math
 import pygame
 
-class map:
-    def __init__(self, start, goal, map_dimensions):
+class Window:
+    def __init__(self, start, goal, map_dimensions, obstacles):
         self.width, self.height = map_dimensions
         self.start = start
         self.goal = goal
-        self.obstacles = []
+        self.obstacles = obstacles
 
-    def draw_obstacle(self, obstacle):
-        self.obstacles.append(obstacle)
+        # pygame settings
+        pygame.display.set_caption("RRT*")
+        self.window = pygame.display.set_mode((self.width, self.height))
+        self.window.fill((0,0,0))
+        self.node_radius = 10
+        self.node_thickness = 0
+        self.edge_thickness = 0
 
-    def draw_map(self, screen):
-        for obstacle in self.obstacles:
-            obstacle.draw(screen)
+    def draw_map(self):
+        # Draw start/goal nodes
+        pygame.draw.circle(self.window, (255, 200, 0), self.start, self.node_radius, self.node_thickness)
+        pygame.draw.circle(self.window, (0, 255, 0), self.goal, self.node_radius, self.node_thickness)
+        # draw obstacles
+        for obs in self.obstacles:
+            pygame.draw.circle(self.window, (255, 255, 255), (obs.x, obs.y), obs.radius)
 
-    def draw_path(self, screen, path):
-        for i in range(len(path) - 1):
-            pygame.draw.line(screen, (0, 255, 0), path[i], path[i + 1], 2)
+
+    def draw_path(self, path):
+        pass
 
 class RRTGraph:
-    def __init__(self):
-        pass
+    def __init__(self, start, goal, map_dimensions):
+        (x, y) = start
+        self.start = start
+        self.goal = goal
+        self.goal_flag = False
+        self.map_dimensions = map_dimensions
+        self.obstacles = []
+        self.goal_flag = False
+        self.path = []
+
+        # Tree data structure: parent-child dictionary. keys are node coordinates, values are parent coordinates
+        tree = {start: start}  # start node is its own parent
+
+    def create_obstacle(self, obstacle):
+        self.obstacles.append(obstacle)
+        return self.obstacles
+
 
     def add_node(self, node):
         pass
@@ -36,4 +60,25 @@ class RRTGraph:
         pass
 
     def remove_edge(self):
+        pass
+
+class Obstacle:
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.trajectory = None
+
+    def set_trajectory(self, trajectory):
+        """
+        Trajectory is stored as a dictionary of time: velocity. Only time stamps 
+        where the velocity changes are stored. Missing time stamps are assumed to
+        have the same velocity as the previous time stamp.
+        """
+        self.trajectory = trajectory
+
+    def get_current_pos(self, time):
+        """
+        Get current position of obstacle
+        """
         pass
--- main.py
+++ main.py
@@ -5,5 +5,36 @@ Author: Michael Zeng
 
 pip install -r requirements.txt
 """
+import pygame
+from RRT import *
+
 
 step_size = 1
+
+
+def main():
+    # Create Obstacles
+    obstacles = []
+    obs1 = Obstacle(600, 400, 100)
+    obs1.set_trajectory({0: 0})
+    obs2 = Obstacle(850, 600, 80)
+    obs2.set_trajectory({0: 0})
+    obstacles.append(obs1)  # 0 velocity at time 0 (and all other times)
+    obstacles.append(obs2)  # 0 velocity at time 0 (and all other times)
+
+    # Initialize pygame window
+    dimensions = (1200, 800)
+    start = (100, 100)
+    goal = (1100, 700)
+    window = Window(start, goal, dimensions, obstacles)
+    window.draw_map()
+
+    pygame.display.update()
+    pygame.event.clear()
+    pygame.event.wait(0)  # freezes window until keypress
+
+
+
+if __name__ == "__main__":
+    print()
+    main()
